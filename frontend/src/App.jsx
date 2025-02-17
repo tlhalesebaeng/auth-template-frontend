733b5869438bbf8e9cd55d837bb0659a532870e9
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 00:39:54 +0200

    Forgot password page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import Login from './pages/authPages/Login';
 import ResetPassword from './pages/authPages/ResetPassword';
+import ForgotPassword from './pages/authPages/ForgotPassword';
 import './App.css';
 
 export default function App() {
-    return <ResetPassword />;
+    return <ForgotPassword />;
 }
--- frontend/src/pages/authPages/ForgotPassword.jsx
+++ frontend/src/pages/authPages/ForgotPassword.jsx
@@ -1,3 +1,20 @@
+import Auth from '../../components/auth-components/Auth';
+import AlternativeAuth from '../../components/auth-components/AlternativeAuth';
+import Input from '../../utils/Input';
+import Button from '../../utils/Button';
+
 export default function ForgotPassword() {
-    return <h1>Forgot your password?</h1>;
+    const description =
+        "Don't worry, enter your email below to reset your password.";
+    return (
+        <Auth title="Forgot your password?" description={description}>
+            <form>
+                <div className="input-container column">
+                    <Input type="email" placeholder="Email" />
+                </div>
+                <Button>Login</Button>
+            </form>
+            <AlternativeAuth alt="Login" />
+        </Auth>
+    );
 }
