c5bf1e653f78936c4e0d4d656445cbe08b5b0386
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-17 22:32:18 +0200

    placing the children prop correctly

--- frontend/src/components/auth-components/Auth.jsx
+++ frontend/src/components/auth-components/Auth.jsx
@@ -4,9 +4,7 @@ export default function Auth({ title, children }) {
     return (
         <main className="auth-card column">
             <h1>{title}</h1>
-            <section>
-                <form>{children}</form>
-            </section>
+            <section>{children}</section>
         </main>
     );
 }
