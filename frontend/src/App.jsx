fd314d24ea2df2e505da09a67f62b2f4e1580b9f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 01:01:49 +0200

    Sign up page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from './pages/authPages/Login';
+import Signup from './pages/authPages/Signup';
 import ResetPassword from './pages/authPages/ResetPassword';
 import ForgotPassword from './pages/authPages/ForgotPassword';
 import CodeVerification from './pages/authPages/CodeVerification';
 import './App.css';
 
 export default function App() {
-    return <CodeVerification />;
+    return <Signup />;
 }
--- frontend/src/pages/authPages/Signup.jsx
+++ frontend/src/pages/authPages/Signup.jsx
@@ -1,3 +1,36 @@
+import AlternativeAuth from '../../components/auth-components/AlternativeAuth';
+import Auth from '../../components/auth-components/Auth';
+import AuthQuestion from '../../components/auth-components/AuthQuestion';
+import RememberMe from '../../components/auth-components/RememberMe';
+import Button from '../../utils/Button';
+import Input from '../../utils/Input';
+
 export default function Signup() {
-    return <h1>Signup</h1>;
+    const description = 'Set up a new account for your profile';
+    return (
+        <Auth title="Sign up" description={description}>
+            <form>
+                <div className="input-container column">
+                    <div className="row">
+                        <Input type="text" placeholder="First Name" />
+                        <Input type="text" placeholder="Last Name" />
+                    </div>
+                    <div className="row">
+                        <Input type="text" placeholder="Email" />
+                        <Input type="text" placeholder="Username" />
+                    </div>
+                    <Input type="password" placeholder="Password" />
+                    <Input type="passwrod" placeholder="Confirm Password" />
+                </div>
+                <AuthQuestion
+                    question="Already have an account?"
+                    option="Login"
+                    name="question-container"
+                >
+                    <Button>Create account</Button>
+                </AuthQuestion>
+            </form>
+            <AlternativeAuth alt="Signup" />
+        </Auth>
+    );
 }
