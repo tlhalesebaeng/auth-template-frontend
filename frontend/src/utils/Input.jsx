a0f756320630550dce459f27a339d62cdcc894fd
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 11:15:11 +0200

    enabling showing and hidding the password

--- frontend/src/utils/Input.jsx
+++ frontend/src/utils/Input.jsx
@@ -1,10 +1,36 @@
+import shownPassword from '../assets/shown-password-logo.png';
+import hiddenPassword from '../assets/hidden-password-logo.png';
 import './Input.css';
+import { useState } from 'react';
 
-export default function Input({ type, placeholder }) {
+export default function Input({ type = 'text', placeholder }) {
+    const [inputType, setInputType] = useState(type);
+    const [inputLogo, setInputLogo] = useState(hiddenPassword);
+
+    function handleInputLogo() {
+        setInputType((prevState) => {
+            if (prevState === 'password') {
+                return 'text';
+            }
+
+            return 'password';
+        });
+        setInputLogo((prevState) => {
+            if (prevState === shownPassword) {
+                return hiddenPassword;
+            }
+
+            return shownPassword;
+        });
+    }
+
     return (
         <div className="input-group">
-            <input type={type} placeholder=" " />
+            <input type={inputType} placeholder=" " />
             <label for="">{placeholder}</label>
+            {type === 'password' && (
+                <img onClick={handleInputLogo} src={inputLogo} />
+            )}
         </div>
     );
 }
