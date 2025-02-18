55abe8218798e14aa0b4516873bd2a5660acc149
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 18:31:51 +0200

    disabling the signup btn when there is an empty field

--- frontend/src/pages/authPages/Signup.jsx
+++ frontend/src/pages/authPages/Signup.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import AlternativeAuth from '../../components/auth-components/AlternativeAuth';
 import Auth from '../../components/auth-components/Auth';
 import AuthQuestion from '../../components/auth-components/AuthQuestion';
@@ -5,40 +6,98 @@ import Button from '../../utils/Button';
 import Input from '../../utils/Input';
 
 export default function Signup() {
-    const description = 'Set up a new account for your profile';
+    const [data, setData] = useState({});
+
     function handleCreateAccount(event) {
         event.preventDefault();
         console.log('Create new account');
     }
+
+    function handleChange(event, type) {
+        setData((prevState) => {
+            return {
+                ...prevState,
+                [type]: event.target.value,
+            };
+        });
+    }
+
+    let disabledButton = false;
+    if (
+        !data.firstName ||
+        !data.lastName ||
+        !data.email ||
+        !data.username ||
+        !data.password ||
+        !data.confirmPassword
+    ) {
+        disabledButton = true;
+    }
+
+    const description = 'Set up a new account for your profile';
+
     return (
         <Auth title="Sign up" description={description}>
             <form>
                 <div className="input-container column">
                     <div className="row">
                         <Input
+                            onChange={(event) => {
+                                handleChange(event, 'firstName');
+                            }}
                             rightMargin="right-margin"
                             type="text"
                             placeholder="First Name"
                         />
-                        <Input type="text" placeholder="Last Name" />
+                        <Input
+                            onChange={(event) => {
+                                handleChange(event, 'lastName');
+                            }}
+                            type="text"
+                            placeholder="Last Name"
+                        />
                     </div>
                     <div className="row">
                         <Input
+                            onChange={(event) => {
+                                handleChange(event, 'email');
+                            }}
                             rightMargin="right-margin"
                             type="text"
                             placeholder="Email"
                         />
-                        <Input type="text" placeholder="Username" />
+                        <Input
+                            onChange={(event) => {
+                                handleChange(event, 'username');
+                            }}
+                            type="text"
+                            placeholder="Username"
+                        />
                     </div>
-                    <Input type="password" placeholder="Password" />
-                    <Input type="password" placeholder="Confirm Password" />
+                    <Input
+                        onChange={(event) => {
+                            handleChange(event, 'password');
+                        }}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <Input
+                        onChange={(event) => {
+                            handleChange(event, 'confirmPassword');
+                        }}
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
                 </div>
                 <AuthQuestion
                     question="Already have an account?"
                     option="Login"
                     name="question-container"
                 >
-                    <Button onClick={handleCreateAccount}>
+                    <Button
+                        disabledButton={disabledButton}
+                        onClick={handleCreateAccount}
+                    >
                         Create account
                     </Button>
                 </AuthQuestion>
