d09b3f696780358c95c7711d8fa0b70ec476c703
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 13:48:34 +0200

    providing basic routes for authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/authPages/Login';
 import Signup from './pages/authPages/Signup';
 import ResetPassword from './pages/authPages/ResetPassword';
@@ -5,6 +6,29 @@ import ForgotPassword from './pages/authPages/ForgotPassword';
 import CodeVerification from './pages/authPages/CodeVerification';
 import './App.css';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+    {
+        path: '/signup',
+        element: <Signup />,
+    },
+    {
+        path: '/users/password/reset',
+        element: <ForgotPassword />,
+    },
+    {
+        path: '/users/password/reset/code',
+        element: <CodeVerification />,
+    },
+    {
+        path: '/users/password/reset/entry',
+        element: <ResetPassword />,
+    },
+]);
+
 export default function App() {
-    return <ForgotPassword />;
+    return <RouterProvider router={router} />;
 }
