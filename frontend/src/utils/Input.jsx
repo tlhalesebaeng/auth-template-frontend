2be92c1afb6752f70e10852e2babeac71d6312ce
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-18 11:22:10 +0200

    disabling toggling on empty string

--- frontend/src/utils/Input.jsx
+++ frontend/src/utils/Input.jsx
@@ -1,32 +1,35 @@
 import shownPassword from '../assets/shown-password-logo.png';
 import hiddenPassword from '../assets/hidden-password-logo.png';
 import './Input.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Input({ type = 'text', placeholder }) {
     const [inputType, setInputType] = useState(type);
     const [inputLogo, setInputLogo] = useState(hiddenPassword);
+    const inputRef = useRef();
 
     function handleInputLogo() {
-        setInputType((prevState) => {
-            if (prevState === 'password') {
-                return 'text';
-            }
+        if (inputRef.current.value !== '') {
+            setInputType((prevState) => {
+                if (prevState === 'password') {
+                    return 'text';
+                }
 
-            return 'password';
-        });
-        setInputLogo((prevState) => {
-            if (prevState === shownPassword) {
-                return hiddenPassword;
-            }
+                return 'password';
+            });
+            setInputLogo((prevState) => {
+                if (prevState === shownPassword) {
+                    return hiddenPassword;
+                }
 
-            return shownPassword;
-        });
+                return shownPassword;
+            });
+        }
     }
 
     return (
         <div className="input-group">
-            <input type={inputType} placeholder=" " />
+            <input ref={inputRef} type={inputType} placeholder=" " />
             <label htmlFor="">{placeholder}</label>
             {type === 'password' && (
                 <img onClick={handleInputLogo} src={inputLogo} />
