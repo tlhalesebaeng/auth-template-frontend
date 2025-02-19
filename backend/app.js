cb800d7fa40b469b77af8ee1f81cbac6ceaea3e1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 13:52:58 +0200

    basic users routing: get a user

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,13 @@ app.get('/quiz/app/api/v1/users', (req, res) => {
     });
 });
 
+app.get('/quiz/app/api/v1/users/:id', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: `get user ${req.params.id}`,
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}..`);
