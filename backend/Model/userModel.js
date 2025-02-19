cdecd2839aa1a870860a1f32e0101259dd61295b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 18:15:09 +0200

    hashing the password

--- backend/Model/userModel.js
+++ backend/Model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -35,6 +36,20 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords do not match',
         },
     },
+});
+
+userSchema.pre('save', async function (next) {
+    //only run this if the password was changed
+    if (!this.isModified('password')) return next();
+
+    //Hash the password
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete the confirm password field
+    this.passwordConfirm = undefined;
+
+    //call the next middleware
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
