81a05bd2077283d5caa0ac9b77e13851d4af1ccf
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 16:30:32 +0200

    using the user model to update the user and delete the user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -49,16 +49,35 @@ exports.createUser = async (req, res) => {
     }
 };
 
-exports.updateUser = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: req.body,
-    });
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({
+            status: 'success',
+            data: user,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
-exports.deleteUser = (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
