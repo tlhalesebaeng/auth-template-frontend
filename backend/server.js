580e6703af8f1614a00a04c0d7fa17d410d5d9ab
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 15:42:46 +0200

    connecting to the database with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+    console.log('DB connection successful!');
+});
 
 const port = 3000;
 app.listen(port, () => {
