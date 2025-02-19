c130990913904376711516999ed430f6e434c0a4
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 14:41:12 +0200

    making the environment variables available in our app using the dotenv package

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
 const usersRoute = require('./routes/userRoutes');
 
 const app = express();
+
+dotenv.config({ path: './config.env' });
 
 app.use(express.json()); //middleware to add the incoming body data to the request object
 app.use(morgan('dev')); //middleware to log some request data
