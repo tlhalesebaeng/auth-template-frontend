9a319a919bb5baf9001d7e2872a07120d74850e4
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 13:54:34 +0200

    basic users routing: create a user

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); //middleware to add the incoming body data to the request object
+
 app.get('/quiz/app/api/v1/users', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -13,6 +15,13 @@ app.get('/quiz/app/api/v1/users/:id', (req, res) => {
     res.status(200).json({
         status: 'success',
         data: `get user ${req.params.id}`,
+    });
+});
+
+app.post('/quiz/app/api/v1/users', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: req.body,
     });
 });
 
