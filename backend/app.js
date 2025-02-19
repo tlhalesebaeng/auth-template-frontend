c84e20c348b1d143d0964162aa847fb43f349737
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 13:50:47 +0200

    basic users routing: getting all users

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/quiz/app/api/v1/users', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: 'all users',
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}..`);
