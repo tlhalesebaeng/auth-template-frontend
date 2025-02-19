e5e39991f2e8595fb5d1c1eb9798c22dbfe2a62b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-19 16:03:15 +0200

    exporting our user model

---
+++ backend/Model/userModel.js
@@ -0,0 +1,28 @@
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'A user must have a first name'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'A user must have a last name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'A user must have an email'],
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: [true, 'A user must have a username'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'A user must have a password'],
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -10,32 +10,6 @@ mongoose.connect(DB).then(() => {
     console.log('DB connection successful!');
 });
 
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, 'A user must have a first name'],
-    },
-    lastName: {
-        type: String,
-        required: [true, 'A user must have a last name'],
-    },
-    email: {
-        type: String,
-        required: [true, 'A user must have an email'],
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: [true, 'A user must have a username'],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, 'A user must have a password'],
-    },
-});
-const User = mongoose.model('User', userSchema);
-
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}..`);
