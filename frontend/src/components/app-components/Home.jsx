9748e8510653b435970cc72bf0e76a23033c2e82
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-22 09:24:32 +0200

    remove the request

--- frontend/src/components/app-components/Home.jsx
+++ frontend/src/components/app-components/Home.jsx
@@ -1,29 +1,7 @@
 import axios from 'axios';
 import { useEffect } from 'react';
+import Cookie from 'cookie-universal';
 
 export default function Home() {
-    useEffect(() => {
-        async function fetch() {
-            try {
-                const response = await axios.get(
-                    'http://127.0.0.1:3000/quiz/app/api/v1/users',
-                    {
-                        withCredentials: true,
-                    }
-                );
-                console.log(response.data);
-            } catch (err) {
-                const responseData = err.response;
-                if (responseData) {
-                    console.log(responseData.data.message);
-                } else {
-                    console.log('Message does not exist');
-                }
-            }
-        }
-
-        fetch();
-    }, []);
-
     return <h1>Home</h1>;
 }
