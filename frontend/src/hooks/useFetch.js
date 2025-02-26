6d52020d640443eac098199c62e4d0a43d37d0c8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-02-26 16:02:16 +0200

    define the res function in way that accormodates the get function

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -17,7 +17,7 @@ const functions = {
 export function useFetch() {
     const [error, setError] = useState('');
 
-    async function res(url, data, method) {
+    async function res(url, method, data = {}) {
         const response = await functions[method](url, data);
 
         if (response.status < 200 || response.status >= 300) {
