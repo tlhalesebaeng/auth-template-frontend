6b3980147a273595e80b97b2975a494dea2e6927
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-17 05:08:56 +0200

    Test whether the home component renders one h1 element and its the wrapper

--- frontend/src/features/Home.test.jsx
+++ frontend/src/features/Home.test.jsx
@@ -3,13 +3,25 @@ import { render, screen } from '@testing-library/react';
 import Home from './Home.jsx';
 
 describe('Home component', () => {
-    it('renders "Home" in an h1 tag', async () => {
+    it('renders only one h1 element', () => {
         render(<Home />);
-        const homeElement = await screen.queryByRole('heading', {
+        const headingElements = screen.getAllByRole('heading');
+        expect(headingElements).toHaveLength(1);
+    });
+
+    it('renders an h1 element as a wrapper', () => {
+        const { container } = render(<Home />);
+        const wrapper = container.firstChild;
+        expect(wrapper.tagName).toBe('H1');
+    });
+
+    it('renders "Home" in an h1 tag', () => {
+        render(<Home />);
+        const homeElement = screen.getByRole('heading', {
             name: /home/i,
             level: 1,
         });
 
-        expect(homeElement).not.toBeNull();
+        expect(homeElement).toBeInTheDocument();
     });
 });
